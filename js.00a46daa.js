a9a220fd62444f153ecc3c5023014a22b610653b
Muhammad Janisar <janisarhyder90@gmail.com>
2025-01-27 12:55:18 +0500

    haha

--- js.00a46daa.js
+++ js.00a46daa.js
@@ -6360,7 +6360,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "4234" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "13601" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
